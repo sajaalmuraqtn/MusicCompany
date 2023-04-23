4ebfd3d9ac6b027c91bfe72228d6826a301a63d3
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2023-04-23 23:04:54 +0300

    navbar change

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var navbar=document.getElementById("navbar");
+var change=document.getElementById("change");
+
+window.addEventListener("scroll",function(){
+    if (window.scrollY>=change.offsetTop) {
+        navbar.style.backgroundColor='rgba(0, 0, 0, 0.596)';
+        navbar.style.boxShadow= "0  0 10px #222"; 
+        navbar.style.color= "black"; 
+       }
+       else{
+        navbar.style.background='none';
+        navbar.style.boxShadow= "none"; 
+       }
+    
+});
